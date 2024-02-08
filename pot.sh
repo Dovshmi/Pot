dc367b682c4bf92f9f5c3620757a03a5d2620be3
dovshmi <dovshmi@gmail.com>
2024-02-08 13:13:11 +0200

    pot V0.2
    pot V0.2 update
    - main loop created
    - massege rewrite
    - empty string edge case debug

--- pot.sh
+++ pot.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-VAL=$HOME/.config/rofi/pot/pothooks.txt
+VAL="$HOME/.config/rofi/pot/pothooks.txt"
 CHECK=$(test -f "$VAL" && echo "true" || echo "false")
 if [[ "$CHECK" == "false" ]]; then
-	dunstify "Cannot find pothooks.txt needs install.sh " -i danger -t 6000
+	dunstify "Cannot find pothooks.txt, needs install.sh " -i danger -t 6000
 	exit
 fi
-VALSORTED=$(sort $VAL | uniq)
-echo "$VALSORTED" | tr ' ' '\n' >$VAL
+VALSORTED=$(sort "$VAL" | uniq)
+echo "$VALSORTED" | tr ' ' '\n' >"$VAL"
+while true; do
+	foo=$(
+		cat "$VAL" | rofi -dmenu -mesg "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
+	)
+	case $foo in
+	"d" | "delete" | "Delete" | "D" | "-")
+		dunstify "Delete Mode " -i danger -t 6000
+		foo=$(cat "$VAL" | rofi -dmenu -mesg "Delete Mode : [ Enter - Delete ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
+		sed -i "\|^${foo}$|d" "$VAL"
+		;;
+	"a" | "add" | "Add" | "A" | "append" | "Append" | "+")
+		dunstify "Append Mode " -i danger -t 6000
+		foo=$(rofi -dmenu -mesg "Append Mode : [ Enter - Append ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
+		if [ -n "$foo" ]; then
+			echo "$foo" >>"$VAL"
+		fi
+		;;
+	"?")
+		echo -e "Normal Mode -> Main hooked paths choosing menu\nDelete Mode -> Delete from hooked paths\nAppend Mode -> Append to hooked paths" |
+			rofi -dmenu -mesg "Help Mode : [Enter - Choose]" -e - -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
+		;;
+	"exit" | "q" | "quit" | ":q" | ":wq" | "")
+		break # Exit the loop
+		;;
+	*)
+		if grep --fixed-string -x "$foo" "$VAL"; then
+			echo "$foo" | xclip -rmlastnl -selection clipboard
+			dunstify "Clip Loaded" -i notification -t 6000
+			break
+		fi
+		;;
+	esac
 
-foo=$(
-	cat $VAL | rofi -dmenu -mesg "Normal Mode : [ D/[-] -> Delete ] | [ A/[+] -> Append ] | [ Enter -> Choose ] | [ ? -> Help ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
-)
-
-case $foo in
-"d" | "delete" | "Delete" | "D" | "-")
-	dunstify "Delete Mode " -i danger -t 6000
-	foo=$(cat $VAL | rofi -dmenu -mesg "Delete Mode : [ Enter - Delete ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
-	sed -i "\|^${foo}$|d" "$VAL"
-	exit
-	;;
-"a" | "add" | "Add" | "A" | "append" | "Append" | "+")
-	dunstify "Append Mode " -i danger -t 6000
-	foo=$(rofi -dmenu -mesg "Append Mode : [ Enter - Append ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
-	if [ -n "$foo" ]; then
-		echo "$foo" >>$VAL
-	fi
-	;;
-"?")
-	echo -e "Normal Mode -> Main hooked paths choosing menu\nDelete Mode -> Delete from hooked paths\nAppend Mode -> Append to hooked paths" |
-		rofi -dmenu -mesg "Help Mode : [Enter - Choose]" -e - -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
-	;;
-esac
-
-if grep --fixed-string -x "$foo" "$VAL"; then
-	echo "$foo" | xclip -rmlastnl -selection clipboard
-	dunstify "Clip Loaded" -i notification -t 6000
-fi
+done
