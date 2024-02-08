13d739c3dd8ba08bcc84532e0895142c3175b23c
Rony Shmidov <70490582+Dovshmi@users.noreply.github.com>
2024-02-08 09:22:53 +0200

    Pot main files Upload V0.1
    V0.1
    upload the main file 
    upload the install.sh script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#installation script for pot -> creating the pot directory and pothocks.txt file
+VAL=$HOME/.config/rofi/pot/pothooks.txt
+VALPATH=$HOME/.config/rofi/pot
+CHECK=$(test -f "$VAL" && echo "true" || echo "false")
+if [[ "$CHECK" == "false" ]]; then
+	echo "starting installations"
+	mkdir $VALPATH
+	echo "$VALPATLH" >pothooks.txt
+fi
