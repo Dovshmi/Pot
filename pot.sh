6df88d544d0667c87662391dea2a4d902044ff58
dovshmi <dovshmi@gmail.com>
2024-02-08 14:55:28 +0200

    pot V0.3
    - Upadate the $VAL values
    - Clean code base

--- pot.sh
+++ pot.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 
-VAL="$HOME/.config/rofi/pot/pothooks.txt"
-CHECK=$(test -f "$VAL" && echo "true" || echo "false")
+HOOKS="$HOME/.config/rofi/pot/pothooks.txt"
+CHECK=$(test -f "$HOOKS" && echo "true" || echo "false")
+THEME="$HOME/.config/rofi/launchers/type-1/style-8.rasi"
+MENU="rofi -dmenu"
+
 if [[ "$CHECK" == "false" ]]; then
 	dunstify "Cannot find pothooks.txt, needs install.sh " -i danger -t 6000
 	exit
 fi
-VALSORTED=$(sort "$VAL" | uniq)
-echo "$VALSORTED" | tr ' ' '\n' >"$VAL"
+
+HOOKSSORTED=$(sort "$HOOKS" | uniq)
+echo "$HOOKSSORTED" | tr ' ' '\n' >"$HOOKS"
+
 while true; do
 	foo=$(
-		cat "$VAL" | rofi -dmenu -mesg "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
+		cat "$HOOKS" | ${MENU} -mesg "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ]" -theme "$THEME"
 	)
 	case $foo in
 	"d" | "delete" | "Delete" | "D" | "-")
 		dunstify "Delete Mode " -i danger -t 6000
-		foo=$(cat "$VAL" | rofi -dmenu -mesg "Delete Mode : [ Enter - Delete ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
-		sed -i "\|^${foo}$|d" "$VAL"
+		foo=$(cat "$HOOKS" | ${MENU} -mesg "Delete Mode : [ Enter - Delete ]" -theme "$THEME")
+		sed -i "\|^${foo}$|d" "$HOOKS"
 		;;
 	"a" | "add" | "Add" | "A" | "append" | "Append" | "+")
 		dunstify "Append Mode " -i danger -t 6000
-		foo=$(rofi -dmenu -mesg "Append Mode : [ Enter - Append ]" -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi")
+		foo=$(${MENU} -mesg "Append Mode : [ Enter - Append ]" -theme "$THEME")
 		if [ -n "$foo" ]; then
-			echo "$foo" >>"$VAL"
+			echo "$foo" >>"$HOOKS"
 		fi
 		;;
 	"?")
 		echo -e "Normal Mode -> Main hooked paths choosing menu\nDelete Mode -> Delete from hooked paths\nAppend Mode -> Append to hooked paths" |
-			rofi -dmenu -mesg "Help Mode : [Enter - Choose]" -e - -theme "$HOME/.config/rofi/launchers/type-1/style-8.rasi"
+			${MENU} -mesg "Help Mode : [Enter - Choose]" -e - -theme "$THEME"
 		;;
 	"exit" | "q" | "quit" | ":q" | ":wq" | "")
 		break # Exit the loop
 		;;
 	*)
-		if grep --fixed-string -x "$foo" "$VAL"; then
+		if grep --fixed-string -x "$foo" "$HOOKS"; then
 			echo "$foo" | xclip -rmlastnl -selection clipboard
 			dunstify "Clip Loaded" -i notification -t 6000
 			break
 		fi
 		;;
 	esac
-
 done
