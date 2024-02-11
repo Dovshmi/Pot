33efe3472a7853cacfe3ceb2cc7929d1206824da
dovshmi <dovshmi@gmail.com>
2024-02-11 21:56:56 +0200

    Pit Version 0.5
    - Updated Main Variables for easy config use
    - Added dmenu functionality
    - Fix dmenu bug in help menu

--- pot.sh
+++ pot.sh
@@ -2,8 +2,13 @@
 
 HOOKS="$HOME/.config/rofi/pot/pothooks.txt"
 CHECK=$(test -f "$HOOKS" && echo "true" || echo "false")
-THEME="$HOME/.config/rofi/launchers/type-1/style-8.rasi"
-MENU="rofi -dmenu"
+
+#TAG if no theme used
+THEME="-theme $HOME/.config/rofi/launchers/type-1/style-8.rasi"
+
+#Untag if using dmenu and tag rofi
+MENU="rofi -dmenu $THEME -mesg "
+#MENU="dmenu -l 100 -p "
 
 if [[ "$CHECK" == "false" ]]; then
 	dunstify "Cannot find pothooks.txt, needs install.sh " -i danger -t 6000
@@ -14,12 +19,12 @@ HOOKSSORTED=$(sort "$HOOKS" | uniq)
 echo "$HOOKSSORTED" | tr ' ' '\n' >"$HOOKS"
 
 delete_mode() {
-	foo=$(cat "$HOOKS" | ${MENU} -mesg "Delete Mode : [ Enter - Delete ] [ESC]" -theme "$THEME")
+	foo=$(cat "$HOOKS" | ${MENU} "Delete Mode : [ Enter - Delete ] [ESC]")
 	sed -i "\|^${foo}$|d" "$HOOKS"
 }
 
 append_mode() {
-	foo=$(${MENU} -mesg "Append Mode : [ Enter - Append ] [ESC]" -theme "$THEME")
+	foo=$(${MENU} "Append Mode : [ Enter - Append ] [ESC]")
 	if [ -n "$foo" ]; then
 		echo "$foo" >>"$HOOKS"
 	fi
@@ -27,7 +32,7 @@ append_mode() {
 
 while true; do
 	foo=$(
-		cat "$HOOKS" | ${MENU} -mesg "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ] [ESC]" -theme "$THEME"
+		cat "$HOOKS" | ${MENU} "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ] [ESC]"
 	)
 	case $foo in
 	"d" | "delete" | "Delete" | "D" | "-")
@@ -40,7 +45,7 @@ while true; do
 		;;
 	"?")
 		HELP=$(echo -e "Normal Mode -> Main hooked paths choosing menu\nDelete Mode -> Delete from hooked paths\nAppend Mode -> Append to hooked paths\nExit -> Exit" |
-			${MENU} -mesg "Help Mode : [Enter - Choose] [ESC]" -e - -theme "$THEME")
+			${MENU} "Help Mode : [Enter - Choose] [ESC]")
 		HELP=$(echo $HELP | awk '{print $1}')
 		case "$HELP" in
 		"Delete")
