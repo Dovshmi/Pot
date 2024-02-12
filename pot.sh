79f6f3ec5ad2f67c5e2c4a27d61b842397bb8991
dovshmi <dovshmi@gmail.com>
2024-02-12 15:57:09 +0200

    Pit Version 0.6
    - Added dunstify function with config options
    - Redesign code structure for dunst
    - Added better command prompt

--- pot.sh
+++ pot.sh
@@ -10,8 +10,14 @@ THEME="-theme $HOME/.config/rofi/launchers/type-1/style-8.rasi"
 MENU="rofi -dmenu $THEME -mesg "
 #MENU="dmenu -l 100 -p "
 
+# Display a message using dunst $1 -> massage for the user $2 -> icon name
+notify() {
+	dunstify "$1" -i "$2" -t 6000
+}
+
 if [[ "$CHECK" == "false" ]]; then
-	dunstify "Cannot find pothooks.txt, needs install.sh " -i danger -t 6000
+	#dunstify "Cannot find pothooks.txt, needs install.sh " -i danger -t 6000
+	notify "Cannot find pothooks.txt, needs install.sh" "danger"
 	exit
 fi
 
@@ -35,26 +41,31 @@ while true; do
 		cat "$HOOKS" | ${MENU} "Normal Mode : [ D/[-] -> Delete ] [ A/[+] -> Append ] [ Enter -> Choose ] [ ? -> Help ] [ESC]"
 	)
 	case $foo in
-	"d" | "delete" | "Delete" | "D" | "-")
-		dunstify "Delete Mode " -i danger -t 6000
+	":d" | "delete" | "Delete" | ":D" | "-")
+		#dunstify "Delete Mode " -i danger -t 6000
+		notify "Delete Mode" "danger"
 		delete_mode
 		;;
-	"a" | "add" | "Add" | "A" | "append" | "Append" | "+")
-		dunstify "Append Mode " -i danger -t 6000
+	":a" | "add" | "Add" | ":A" | "append" | "Append" | "+")
+		#dunstify "Append Mode " -i danger -t 6000
+		notify "Append Mode" "danger"
 		append_mode
 		;;
-	"?")
+	"?" | ":?" | "help" | "HELP")
 		HELP=$(echo -e "Normal Mode -> Main hooked paths choosing menu\nDelete Mode -> Delete from hooked paths\nAppend Mode -> Append to hooked paths\nExit -> Exit" |
 			${MENU} "Help Mode : [Enter - Choose] [ESC]")
 		HELP=$(echo $HELP | awk '{print $1}')
 		case "$HELP" in
 		"Delete")
-			dunstify "Delete Mode " -i danger -t 6000
+			#dunstify "Delete Mode " -i danger -t 6000
+			notify "Delete Mode" "danger"
 			delete_mode
 
 			;;
 		"Append")
-			dunstify "Append Mode " -i danger -t 6000
+			#dunstify "Append Mode " -i danger -t 6000
+			notify "Append Mode" "danger"
+
 			append_mode
 			;;
 		"Exit")
@@ -71,7 +82,8 @@ while true; do
 	*)
 		if grep --fixed-string -x "$foo" "$HOOKS"; then
 			echo "$foo" | xclip -rmlastnl -selection clipboard
-			dunstify "Clip Loaded" -i notification -t 6000
+			#dunstify "Clip Loaded" -i notification -t 6000
+			notify "clipboard Loaded" "notification"
 			break
 		fi
 		;;
